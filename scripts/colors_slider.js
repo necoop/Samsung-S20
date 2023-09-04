713a2b248983f05a18ee0a5cc930621b1230651b
Андрей Мокеев <amok2005@gmail.com>
2023-09-04 22:11:48 +0300

    color v 0.3

--- scripts/colors_slider.js
+++ scripts/colors_slider.js
@@ -5,6 +5,12 @@ let activeSlide = 2;
 //Задаём активный цвет
 let activeColor = 0;
 
+//Список координат занимаемых слайдами
+const coordList = [
+  -570, -380, -190, 0, 190, 380, 750, 940, 1130, 1320, 1510, 1700, 1890, 2080,
+  2270, 2460, 2650, 2840,
+];
+
 //Выбираем активный цвет
 colors.on("click", function () {
   colors.each(function (index, element) {
@@ -19,7 +25,7 @@ colors.on("click", function () {
 });
 
 //Первоначальная расстановка слайдов
-for (let j = 0; j < 2; j++) {
+for (let j = 0; j < 3; j++) {
   for (let i = 0; i < 6; i++) {
     $(".carusel").append(
       `<li><img src="./img/view/color1_${i + 1}.png" alt="Фото телефона цвет ${
@@ -29,5 +35,16 @@ for (let j = 0; j < 2; j++) {
   }
 }
 let caruselItem = $(".carusel li");
-caruselItem.eq(activeSlide).addClass('active');
-console.log(caruselItem);
+caruselItem.eq(activeSlide).addClass("active");
+for (let i = 0; i < caruselItem.length; i++) {
+  caruselItem
+    .eq(i)
+    .css(
+      "transform",
+      `translateX(${coordList[(i + 5 - activeSlide) % caruselItem.length]}px)`
+    );
+}
+
+caruselItem.on('click', function(event){
+  console.log(caruselItem.index(event.target.closest('.carusel li')))
+})
