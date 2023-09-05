db06322c77fda2d00f7bfbf9d12de4aa8a2a63ba
Андрей Мокеев <amok2005@gmail.com>
2023-09-05 14:44:31 +0300

    добавление скролла по щелчку для слайдера цвета

--- scripts/colors_slider.js
+++ scripts/colors_slider.js
@@ -1,14 +1,44 @@
+function slideUp() {
+  caruselItem.css('transition', '.3s ease all');
+  for (let i = 0; i < caruselItem.length; i++) {
+    let currentCoord = coordList[((3 - activeSlide) + i + caruselItem.length) % caruselItem.length];
+    caruselItem
+      .eq(i)
+      .css(
+        "transform",
+        `translateX(${currentCoord}px)`
+      );
+    //Ставим класс active
+    if (currentCoord === 380) {
+      caruselItem.eq(i).addClass('active');
+    } else {
+      caruselItem.eq(i).removeClass('active');
+    }
+    //Устанавливаем невидимость
+    if (currentCoord === 2460 || currentCoord === 2650 || currentCoord === -760 || currentCoord === -570) {
+      if (caruselItem.eq(i).css('visibility') != 'hidden') {
+        caruselItem.eq(i).css('visibility', 'hidden');
+      }
+    } else if (caruselItem.eq(i).css('visibility') === 'hidden') {
+      caruselItem.eq(i).css('visibility', 'visible');
+    }
+  }
+  caruselItem.on('transitionend', function () {
+    caruselItem.css('transition', 'none');
+  })
+}
+
 const colors = $(".colors li");
 colors.eq(0).addClass("active");
-let activeSlide = 2;
+let activeSlide = 3;
 
 //Задаём активный цвет
 let activeColor = 0;
 
 //Список координат занимаемых слайдами
 const coordList = [
-  -570, -380, -190, 0, 190, 380, 750, 940, 1130, 1320, 1510, 1700, 1890, 2080,
-  2270, 2460, 2650, 2840,
+  0, 190, 380, 750, 940, 1130, 1320, 1510, 1700, 1890, 2080,
+  2270, 2460, 2650, -760, -570, -380, -190
 ];
 
 //Выбираем активный цвет
@@ -28,23 +58,20 @@ colors.on("click", function () {
 for (let j = 0; j < 3; j++) {
   for (let i = 0; i < 6; i++) {
     $(".carusel").append(
-      `<li><img src="./img/view/color1_${i + 1}.png" alt="Фото телефона цвет ${
-        activeColor + 1
+      `<li><img src="./img/view/color1_${i + 1}.png" alt="Фото телефона цвет ${activeColor + 1
       } вид ${i + 1}" draggable="false"></li>`
     );
   }
 }
 let caruselItem = $(".carusel li");
-caruselItem.eq(activeSlide).addClass("active");
-for (let i = 0; i < caruselItem.length; i++) {
-  caruselItem
-    .eq(i)
-    .css(
-      "transform",
-      `translateX(${coordList[(i + 5 - activeSlide) % caruselItem.length]}px)`
-    );
-}
+slideUp();
 
-caruselItem.on('click', function(event){
-  console.log(caruselItem.index(event.target.closest('.carusel li')))
+caruselItem.on('click', function (event) {
+  let deltaActiveSlide = 0;
+  if (getTranslateX($(this)) === 0) deltaActiveSlide = -2;
+  if (getTranslateX($(this)) === 190) deltaActiveSlide = -1;
+  if (getTranslateX($(this)) === 750) deltaActiveSlide = 1;
+  if (getTranslateX($(this)) === 940) deltaActiveSlide = 2;
+  activeSlide = (activeSlide + deltaActiveSlide + caruselItem.length) % caruselItem.length;
+  slideUp();
 })
